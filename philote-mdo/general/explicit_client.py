30ed1efd5f60a63563ab0abc3acf7451a4d33b78
Christopher Lupp <christopherlupp@gmail.com>
2023-03-10 20:30:06 -0600

    added explicit member variables for meta data

--- philote-mdo/general/explicit_client.py
+++ philote-mdo/general/explicit_client.py
@@ -1,6 +1,41 @@
+import numpy as np
 
 
 class ExplicitClient():
+    """
+    Client for calling explicit analysis discipline servers.
+    """
 
-    def __init__():
+    def __init__(self):
+        # continuous inputs (names, shapes, units)
+        self._vars = []
+        self._vars_shape = {}
+        self._vars_units = {}
+
+        # discrete inputs (names, shapes, units)
+        self._discrete_vars = []
+        self._discrete_vars_shape = {}
+        self._discrete_vars_units = {}
+
+        # continous outputs (names, shapes, units)
+        self._func = []
+        self._func_shape = {}
+        self._func_units = {}
+
+        # discrete outputs (names, shapes, units)
+        self._discrete_func = []
+        self._discrete_func_shape = {}
+        self._discrete_func_units = {}
+
+    def remote_setup(self):
         pass
+
+    def remote_compute(self, inputs, outputs):
+        array = np.array([])
+        shape = list(array.shape)
+        data = array.flatten().tolist()
+
+    def remote_partials(self, inputs, jacobian):
+        array = np.array([])
+        shape = list(array.shape)
+        data = array.flatten().tolist()
