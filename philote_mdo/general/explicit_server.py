c16c8d193680744cb943c3b7889b5b2b7adce0b1
Christopher Lupp <christopherlupp@gmail.com>
2023-03-25 18:27:27 -0400

    implemented server setup function

--- philote_mdo/general/explicit_server.py
+++ philote_mdo/general/explicit_server.py
@@ -1,6 +1,7 @@
 import grpc
 import explicit_pb2
 import explicit_pb2_grpc
+import metadata_pb2
 
 
 class ExplicitServer(explicit_pb2_grpc.ExplicitComponentServicer):
@@ -23,11 +24,50 @@ class ExplicitServer(explicit_pb2_grpc.ExplicitComponentServicer):
         self.num_double = request.num_double
         self.num_int = request.num_int
 
+        # continuous inputs (names, shapes, units)
+        self._vars = []
+
+        # discrete inputs (names, shapes, units)
+        self._discrete_vars = []
+
+        # continous outputs (names, shapes, units)
+        self._funcs = []
+
+        # discrete outputs (names, shapes, units)
+        self._discrete_funcs = []
+
     def Setup(self, request, context):
         """
         Transmits setup information about the analysis discipline to the client.
         """
-        pass
+        # transmit the continuous input metadata
+        for var in self._vars:
+            yield metadata_pb2.VariableMetaData(discrete=False,
+                                                input=True,
+                                                name=var['name'],
+                                                shape=var['shape'],
+                                                units=var['units'])
+        # transmit the discrete input metadata
+        for var in self._discrete_vars:
+            yield metadata_pb2.VariableMetaData(discrete=True,
+                                                input=True,
+                                                name=var['name'],
+                                                shape=var['shape'],
+                                                units=var['units'])
+        # transmit the continuous output metadata
+        for func in self._funcs:
+            yield metadata_pb2.VariableMetaData(discrete=False,
+                                                input=False,
+                                                name=func['name'],
+                                                shape=func['shape'],
+                                                units=func['units'])
+        # transmit the discrete output metadata
+        for func in self._discrete_funcs:
+            yield metadata_pb2.VariableMetaData(discrete=True,
+                                                input=False,
+                                                name=func['name'],
+                                                shape=func['shape'],
+                                                units=func['units'])
 
     def Compute(self, request_iterator, context):
         """
