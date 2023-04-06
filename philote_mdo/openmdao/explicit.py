e240bddc69038ca11ad3df7cd3d0e52e482aa066
Christopher Lupp <christopherlupp@gmail.com>
2023-04-05 22:28:31 -0400

    initial implementation of the openMDAO client

--- philote_mdo/openmdao/explicit.py
+++ philote_mdo/openmdao/explicit.py
@@ -16,22 +16,66 @@ import openmdao.api as om
 import philote_mdo as pm
 
 
-class RemoteExplicitComponent(om.ExplicitComponent, pm.ExplicitServer):
+class RemoteExplicitComponent(om.ExplicitComponent, pm.ExplicitClient):
     """
     An OpenMDAO component that acts as a client to an explicit analysis server.
     """
 
+    def initialize(self):
+        # host
+        self.options.declare('host', default='localhost')
+
     def setup(self):
-        pass
+        # assign the host
+        self._host = self.options['host']
+
+        # create the connection to the server
+        self.__connect_host()
+
+        # set up the remote server
+        self._remote_setup()
+
+        # define inputs
+        for input in self._vars:
+            self.add_input(input['name'], shape=input['shape'],
+                           units=input['units'])
+
+        # define discrete inputs
+        for dinput in self._vars:
+            self.add_discrete_input(dinput['name'], shape=dinput['shape'],
+                                    units=dinput['units'])
+
+        # define outputs
+        for out in self._funcs:
+            self.add_output(input['name'], shape=input['shape'],
+                            units=input['units'])
+
+        # define discrete outputs
+        for dout in self._funcs:
+            self.add_discrete_output(input['name'], shape=input['shape'],
+                                     units=input['units'])
 
     def setup_partials(self):
-        pass
-
-    def configure(self):
-        pass
+        # setup the partials on the server
+        self._setup_remote_partials()
 
     def compute(self, inputs, discrete_inputs, outputs, discrete_outputs):
-        pass
+        # call the remote compute method
+        out, discrete_out = self._remote_compute(inputs, discrete_inputs,
+                                                 discrete_outputs)
 
-    def compute_partials(self, inputs, discrete_inputs, J):
-        pass
+        # assign the values to the openmdao dict type
+        for key, val in out.items():
+            outputs[key] = val
+
+        # assign the values to the openmdao dict type
+        for key, val in discrete_out.items():
+            discrete_outputs[key] = val
+
+    def compute_partials(self, inputs, discrete_inputs, partials):
+        # call the remote analysis to get the partials
+        jac = self._remote_compute_partials(inputs, discrete_inputs)
+
+        # assign the values to the openmdao partials type
+        for key, val in jac.items():
+            partials[key] = val
