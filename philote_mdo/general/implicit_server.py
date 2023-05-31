e5b945abe2af32a4e6d6e4529cea09c941665889
Christopher Lupp <christopherlupp@gmail.com>
2023-05-31 16:41:42 -0400

    added implicit discipline inheritance

--- philote_mdo/general/implicit_server.py
+++ philote_mdo/general/implicit_server.py
@@ -11,32 +11,148 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import philote_mdo.generated.implicit_pb2_grpc as implicit_pb2_grpc
+import philote_mdo.generated.array_pb2 as array_pb2
+from philote_mdo.general.server_base import ServerBase
+from philote_mdo.utils import get_chunk_indicies
 
 
-class ImplicitServer():
+class ImplicitServer(ServerBase, implicit_pb2_grpc.ImplicitDisciplineServicer):
     """
     Base class for creating an implicit discipline server.
     """
 
     def __init__(self):
-        """
-        """
-        pass
+        super().__init__()
 
     def Residuals(self, request_iterator, context):
         """
+        Computes the residuals and sends the results to the client.
         """
-        pass
+        # inputs and outputs
+        inputs = {}
+        flat_inputs = {}
+        discrete_inputs = {}
+        flat_disc = {}
+        outputs = {}
+        discrete_outputs = {}
+
+        # preallocate the input and discrete input arrays
+        self.preallocate_inputs(inputs, flat_inputs,
+                                discrete_inputs, flat_disc)
+
+        # process inputs
+        self.process_inputs(request_iterator, flat_inputs, flat_disc)
+
+        # call the user-defined compute function
+        self.apply_nonlinear(
+            inputs, outputs, discrete_inputs, discrete_outputs)
+
+        # iterate through all continuous outputs in the dictionary
+        for output_name, value in outputs.items():
+
+            # iterate through all chunks needed for the current input
+            for b, e in get_chunk_indicies(value.size, self.num_double):
+                # create the chunked data
+                yield array_pb2.Array(name=output_name,
+                                      start=b,
+                                      end=e,
+                                      continuous=value.ravel()[b:e])
+
+        # iterate through all discrete outputs in the dictionary
+        for doutput_name, value in discrete_outputs.items():
+
+            # iterate through all chunks needed for the current input
+            for b, e in get_chunk_indicies(value.size, self.num_double):
+                # create the chunked data
+                yield array_pb2.Array(name=doutput_name,
+                                      start=b,
+                                      end=e,
+                                      discrete=value.ravel()[b:e])
 
     def Solve(self, request_iterator, context):
         """
         """
-        pass
+        # inputs and outputs
+        inputs = {}
+        flat_inputs = {}
+        discrete_inputs = {}
+        flat_disc = {}
+        outputs = {}
+        discrete_outputs = {}
+
+        # preallocate the input and discrete input arrays
+        self.preallocate_inputs(inputs, flat_inputs,
+                                discrete_inputs, flat_disc)
+
+        # process inputs
+        self.process_inputs(request_iterator, flat_inputs, flat_disc)
+
+        # call the user-defined compute function
+        self.solve_nonlinear(
+            inputs, outputs, discrete_inputs, discrete_outputs)
+
+        # iterate through all continuous outputs in the dictionary
+        for output_name, value in outputs.items():
+
+            # iterate through all chunks needed for the current input
+            for b, e in get_chunk_indicies(value.size, self.num_double):
+                # create the chunked data
+                yield array_pb2.Array(name=output_name,
+                                      start=b,
+                                      end=e,
+                                      continuous=value.ravel()[b:e])
+
+        # iterate through all discrete outputs in the dictionary
+        for doutput_name, value in discrete_outputs.items():
+
+            # iterate through all chunks needed for the current input
+            for b, e in get_chunk_indicies(value.size, self.num_double):
+                # create the chunked data
+                yield array_pb2.Array(name=doutput_name,
+                                      start=b,
+                                      end=e,
+                                      discrete=value.ravel()[b:e])
 
     def ResidualGradients(self, request_iterator, context):
         """
+        Computes the residual gradients and sends the results to the client.
         """
-        pass
+        # inputs and outputs
+        inputs = {}
+        flat_inputs = {}
+        discrete_inputs = {}
+        flat_disc = {}
+
+        # preallocate the input and discrete input arrays
+        self.preallocate_inputs(inputs, flat_inputs,
+                                discrete_inputs, flat_disc)
+
+        # preallocate the partials
+        jac = self.preallocate_partials()
+
+        # process inputs
+        self.process_inputs(request_iterator, flat_inputs, flat_disc)
+
+        # call the user-defined compute_partials function
+        self.linearize(inputs, jac, discrete_inputs)
+
+        # iterate through all continuous outputs in the dictionary
+        for jac, value in jac.items():
+
+            # iterate through all chunks needed for the current input
+            for b, e in get_chunk_indicies(value.size, self.num_double):
+                # create and send the chunked data
+                yield array_pb2.Array(name=jac[0],
+                                      subname=jac[1],
+                                      start=b,
+                                      end=e,
+                                      continuous=value.ravel()[b:e])
+
+    # def MatrixFreeGradients(self, request_iterator, context):
+    #     """
+    #     """
+    #     pass
 
     # user defined functions (will be overloaded)
 
