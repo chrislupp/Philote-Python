dc5253975d98a4fe6547ef63e65465822040266e
Christopher Lupp <christopherlupp@gmail.com>
2023-10-09 08:38:05 -0400

    removed example classes from run scripts

--- examples/parabaloid_explicit.py
+++ examples/parabaloid_explicit.py
@@ -14,29 +14,7 @@
 import philote_mdo as pmdo
 
 
-class RemoteParabaloid(pmdo.general.ExplicitServer):
 
-    def setup(self):
-        self.define_input('x', shape=(1,), units='m')
-        self.define_input('y', shape=(1,), units='m')
-
-        self.define_output('f_xy', shape=(1,), units='m**2')
-
-    def setup_partials(self):
-        self.define_partials('f_xy', '*')
-
-    def compute(self, inputs, outputs):
-        x = inputs['x']
-        y = inputs['y']
-
-        outputs['f_xy'] = (x - 3.0)**2 + x * y + (y + 4.0)**2 - 3.0
-
-    def compute_partials(self, inputs, partials):
-        x = inputs['x']
-        y = inputs['y']
-
-        partials['f_xy', 'x'] = 2.0 * x - 6.0 + y
-        partials['f_xy', 'y'] = 2.0 * y + 8.0 + x
 
 
 pmdo.run_server(RemoteParabaloid())
