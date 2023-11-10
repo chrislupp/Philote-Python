9b90f187b8a8f67704749c4831ad02d1b11bee36
Christopher Lupp <christopherlupp@gmail.com>
2023-11-10 09:17:56 -0500

    updated banners with PA number

--- philote_mdo/examples/paraboloid.py
+++ philote_mdo/examples/paraboloid.py
@@ -16,7 +16,7 @@
 #
 #
 # This work has been cleared for public release, distribution unlimited, case
-# number: AFRL-2023-XXXX.
+# number: AFRL-2023-5713.
 #
 # The views expressed are those of the authors and do not reflect the
 # official guidance or position of the United States Government, the
