d018bc29e6a9f451f34fa421994d13411e0c1f33
Christopher Lupp <christopherlupp@gmail.com>
2023-11-10 09:21:36 -0500

    bumped package version

--- philote_mdo/__init__.py
+++ philote_mdo/__init__.py
@@ -27,6 +27,6 @@
 # the linked websites, of the information, products, or services contained
 # therein. The DoD does not exercise any editorial, security, or other
 # control over the information you may find at these locations.
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
 __all__ = ["__version__"]
