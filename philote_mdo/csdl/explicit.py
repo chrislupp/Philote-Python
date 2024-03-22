d184dca68156912fa27d023ce96f61c3de96013d
Christopher Lupp <christopherlupp@gmail.com>
2024-03-21 21:26:28 -0400

    started CSDL explicit operation

--- philote_mdo/csdl/explicit.py
+++ philote_mdo/csdl/explicit.py
@@ -27,3 +27,89 @@
 # the linked websites, of the information, products, or services contained
 # therein. The DoD does not exercise any editorial, security, or other
 # control over the information you may find at these locations.
+try:
+    import csdl
+    csdl_installed = True
+except ImportError:
+    csdl_installed = False
+    om = None
+import philote_mdo.general as pm
+import philote_mdo.openmdao.utils as utils
+
+
+class RemoteExplicitOperation(csdl.CustomExplicitOperation):
+    """
+    Custom explicit operation that interfaces with a Philote explicit component.
+    """
+
+    def __init__(self, channel=None, num_par_fd=1, **kwargs):
+        """
+        Initialize the component and client.
+        """
+        if not channel:
+            raise ValueError('No channel provided, the Philote client will not'
+                             'be able to connect.')
+
+        # generic Philote client
+        # The setting of OpenMDAO options requires the list of available
+        # Philote discipline options to be known during initialize. That
+        # means that the server must be reachable to query the
+        # available options on this discipline.
+        self._client = pm.ExplicitClient(channel=channel)
+
+        # call the init function of the explicit component
+        super().__init__(num_par_fd=1, **kwargs)
+
+        # assign and send the option values to the server
+        # this must be done here and not in initialize, as the values of the
+        # OpenMDAO options are only set after intialize has been called in the
+        # init function. That is why the parent init function must be called
+        # before sending the options values to the philote server.
+        # options = {}
+        # for key, val in self.options.items():
+        #     options[key] = val
+        self._client.send_options(kwargs)
+
+    def initialize(self):
+        """
+        Define the OpenMDAO component options.
+        """
+        # get the available options from the philote discipline
+        self._client.get_available_options()
+
+        # add to the OpenMDAO component options
+        for name, type_str in self._client.options_list.items():
+            type = None
+            if type_str == 'bool':
+                type = bool
+            elif type_str == 'int':
+                type = int
+            elif type_str == 'float':
+                type = float
+            elif type_str == 'str':
+                type = str
+
+            self.parameters.declare(name, types=type)
+
+    def define(self):
+        """
+        Set up the OpenMDAO component.
+        """
+        utils.client_setup(self)
+        utils.client_setup_partials(self)
+
+    def compute(self, inputs, outputs):
+        """
+        Compute the function evaluation.
+        """
+        local_inputs = utils.create_local_inputs(inputs, self._client._var_meta)
+        out = self._client.run_compute(local_inputs)
+        utils.assign_global_outputs(out, outputs)
+
+    def compute_derivatives(self, inputs, derivatives):
+        """
+        Compute the gradient evaluation.
+        """
+        local_inputs = utils.create_local_inputs(inputs, self._client._var_meta)
+        jac = self._client.run_compute_partials(local_inputs)
+        utils.assign_global_outputs(jac, derivatives)
